b295fd57a0804515212e27fa7c0b93d4ee7f4fa3
Armin Dzibric <armindzibric6@gmail.com>
2021-04-30 17:46:29 +0200

    Show/hide order sections

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
 function showInformationSection() {
 
-    document.getElementById("customer-info").style.visibility = "visible";
-    document.getElementById("payment-section").style.visibility = "hidden";
-    document.getElementById("confirm-section").style.visibility = "hidden";
+    document.getElementById("info-section").style.display = "flex";
+    document.getElementById("payment-section").style.display = "none";
+    document.getElementById("confirm-section").style.display = "none";
 
-
+    document.getElementById("section-info-btn").style.color = "#333";
+    document.getElementById("section-payment-btn").style.color = "rgb(180, 179, 179)";
+    document.getElementById("section-confirm-btn").style.color = "rgb(180, 179, 179)";
 }
 
 function showPaymentSection() {
-    document.getElementById("payment-section").style.visibility = "visible";
-    document.getElementById("customer-info").style.visibility = "hidden";
-    document.getElementById("confirm-section").style.visibility = "hidden";
+    document.getElementById("payment-section").style.display = "initial";
+    document.getElementById("info-section").style.display = "none";
+    document.getElementById("confirm-section").style.display = "none";
+    document.getElementById("section-payment-btn").style.color = "#333";
+    document.getElementById("section-confirm-btn").style.color = "rgb(180, 179, 179)";
+    document.getElementById("section-info-btn").style.color = "rgb(180, 179, 179)";
 }
 
 function showConfirmSection() {
-    document.getElementById("confirm-section").style.visibility = "visible";
-    document.getElementById("customer-info").style.visibility = "hidden";
-    document.getElementById("payment-section").style.visibility = "hidden";
+    document.getElementById("confirm-section").style.display = "flex";
+    document.getElementById("info-section").style.display = "none";
+    document.getElementById("payment-section").style.display = "none";
+
+    document.getElementById("section-confirm-btn").style.color = "#333";
+    document.getElementById("section-payment-btn").style.color = "rgb(180, 179, 179)";
+    document.getElementById("section-info-btn").style.color = "rgb(180, 179, 179)";
+
 }
