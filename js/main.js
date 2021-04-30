59a1be254804ac92d1066b4f03a7c3e8108b618d
Armin Dzibric <armindzibric6@gmail.com>
2021-04-30 22:44:08 +0200

    Order confirmation

--- js/main.js
+++ js/main.js
@@ -28,3 +28,15 @@ function showConfirmSection() {
     document.getElementById("section-info-btn").style.color = "rgb(180, 179, 179)";
 
 }
+
+function showOrderConfirmationMsg() {
+    document.getElementById("order-confirmation-message").style.display = "initial";
+    document.getElementById("confirm-section").style.display = "none";
+    document.getElementById("info-section").style.display = "none";
+    document.getElementById("payment-section").style.display = "none";
+
+    document.getElementById("section-confirm-btn").style.color = "#333";
+    document.getElementById("section-payment-btn").style.color = "rgb(180, 179, 179)";
+    document.getElementById("section-info-btn").style.color = "rgb(180, 179, 179)";
+
+}
